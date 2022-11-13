09b86589685693dceed5c6bedb54298b22a7f5cb
Ulisses22 <ulissesnetofreitasalves@gmail.com>
2022-11-13 01:40:52 +0000

    Done filter and delete

--- app/src/app/all-students/all-students.component.ts
+++ app/src/app/all-students/all-students.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
 import { Student } from '../student';
 import { StudentsService } from '../students.service';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteDialogStudentComponent } from '../delete-dialog-student/delete-dialog-student.component';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { Subject, takeUntil } from 'rxjs';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { userInfo } from 'os';
 
  
 @Component({
@@ -22,22 +25,35 @@ export class AllStudentsComponent implements OnInit {
     'age',
     'actions',
   ];
+  dataSource!: MatTableDataSource<Student>
+  changeDetectorRefs: any;
 
   constructor(
     private studentService: StudentsService,
     public dialog: MatDialog,
 
   ) {}
+  @ViewChild(MatSort, { static: true }) sort: MatSort = new MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
   ngOnInit(): void {
     this.get();
   }
+  ngOnDestroy() {
+
+  }
 
   get() {
-    this.studentService.get().subscribe((data) => {
-      this.allStudentsSource = data  
-    });
-    
+    this.studentService.get().pipe(takeUntil(this.destroy$)).subscribe((data)=>{
+      this.allStudentsSource = data
+
+      // paginator
+      this.dataSource = new MatTableDataSource(this.allStudentsSource)
+      this.dataSource.sort = this.sort
+      this.dataSource.paginator = this.paginator
+      
+    })
   }
  
   openDeleteModal(id: number) {
@@ -48,10 +64,16 @@ export class AllStudentsComponent implements OnInit {
  
     dialogRef.afterClosed().subscribe((result) => {
       if (result) {
-        this.allStudentsSource = this.allStudentsSource.filter(
-          (_) => _.id !== id
+        this.dataSource.data = this.dataSource.data.filter(
+          (u) => u.id !== id
         );
       }
     });
+  }
+
+
+  applyFilter(event: any) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLocaleLowerCase();
   }
 }
--- app/src/app/delete-dialog-student/delete-dialog-student.component.ts
+++ app/src/app/delete-dialog-student/delete-dialog-student.component.ts
@@ -16,6 +16,7 @@ export class DeleteDialogStudentComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+
   }
 
   confirmDelete() {
